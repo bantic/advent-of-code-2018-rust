31541f8a91c091bdee4d28a874b2810281c999ce
Cory <cory.forsyth@gmail.com>
2019-03-05 13:05:54 -0500

    day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -14,13 +14,19 @@ pub fn run() {
   let contents = include_str!("../data/day6.txt");
   let points = prepare_input(contents);
   println!("day 6 part 1: {}", part1(&points));
+  println!("day 6 part 2: {}", part2(&points));
 }
 
-fn part1(points: &Vec<Point2>) -> u32 {
+fn get_bounds(points: &Vec<Point2>) -> (u32, u32, u32, u32) {
   let xmin = points.iter().min_by_key(|p| p.x).unwrap().x;
   let xmax = points.iter().max_by_key(|p| p.x).unwrap().x;
   let ymin = points.iter().min_by_key(|p| p.y).unwrap().y;
   let ymax = points.iter().max_by_key(|p| p.y).unwrap().y;
+  (xmin, xmax, ymin, ymax)
+}
+
+fn part1(points: &Vec<Point2>) -> u32 {
+  let (xmin, xmax, ymin, ymax) = get_bounds(points);
 
   // keep track of total owned squares for each point
   // keep track of which points' extents touch the grid edge, to disregard them
@@ -68,4 +74,28 @@ fn part1(points: &Vec<Point2>) -> u32 {
   }
 
   max_area
+}
+
+fn total_distance(x: u32, y: u32, points: &Vec<Point2>) -> u32 {
+  let mut total = 0;
+  let cur = Point2::new(x, y);
+  for point in points {
+    total += cur.manhattan_dist(point);
+  }
+  total
+}
+
+fn part2(points: &Vec<Point2>) -> u32 {
+  const MIN_REGION_DIST: u32 = 10000;
+  let (xmin, xmax, ymin, ymax) = get_bounds(points);
+  let mut sum = 0;
+  for x in xmin..=xmax {
+    for y in ymin..=ymax {
+      let dist = total_distance(x, y, points);
+      if dist < MIN_REGION_DIST {
+        sum += 1;
+      }
+    }
+  }
+  sum
 }
