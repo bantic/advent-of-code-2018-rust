d1996c38ce7efd25f2c3e8ec77980696f7a02235
Cory <cory.forsyth@gmail.com>
2019-03-11 16:47:05 -0400

    day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -19,6 +19,7 @@ pub fn run() {
     dep_map.entry(dep.dep).or_default();
   }
   println!("day 7 part 1: {}", part1(&dep_map));
+  println!("day 7 part 2: {:?}", part2(&dep_map));
 }
 
 fn parse_line(l: &str) -> Dependency {
@@ -55,4 +56,144 @@ fn part1(dep_map: &DependencyMap) -> String {
   }
 
   used.iter().collect::<String>()
+}
+
+struct Worker {
+  elapsed: u8,
+  cur: Option<char>,
+}
+
+impl Worker {
+  fn new() -> Worker {
+    Worker {
+      elapsed: 0,
+      cur: None,
+    }
+  }
+
+  fn work_time(c: char) -> u8 {
+    60 + 1 + (c as u8) - b'A'
+  }
+
+  fn tick(&mut self) {
+    self.elapsed += 1;
+  }
+
+  fn can_complete(&self) -> bool {
+    if let Some(cur) = self.cur {
+      let needed = Worker::work_time(cur);
+      self.elapsed >= needed
+    } else {
+      false
+    }
+  }
+
+  fn complete(&mut self) -> char {
+    if !self.can_complete() {
+      panic!("cannot complete");
+    }
+    let ch = self.cur.unwrap();
+    self.cur = None;
+    self.elapsed = 0;
+    ch
+  }
+
+  fn can_work(&self) -> bool {
+    self.cur == None
+  }
+
+  fn is_working(&self) -> bool {
+    self.cur != None
+  }
+
+  fn work(&mut self, c: char) {
+    self.elapsed = 0;
+    self.cur = Some(c);
+  }
+}
+
+struct Pool {
+  elapsed: u32,
+  workers: Vec<Worker>,
+}
+
+impl Pool {
+  fn new(count: usize) -> Pool {
+    let mut workers = vec![];
+    for _ in 0..count {
+      workers.push(Worker::new());
+    }
+    Pool {
+      elapsed: 0,
+      workers,
+    }
+  }
+
+  fn available_workers(&self) -> usize {
+    self.workers.iter().filter(|w| w.can_work()).count()
+  }
+
+  fn can_work(&self) -> bool {
+    self.available_workers() > 0
+  }
+
+  fn work(&mut self, value: char) {
+    let mut did_work = false;
+    for worker in &mut self.workers {
+      if worker.can_work() {
+        worker.work(value);
+        did_work = true;
+        break;
+      }
+    }
+    if !did_work {
+      panic!("could not work");
+    }
+  }
+
+  fn tick(&mut self) -> Vec<char> {
+    self.elapsed += 1;
+    let mut completed = vec![];
+    for worker in &mut self.workers {
+      if !worker.is_working() {
+        continue;
+      }
+      worker.tick();
+      if worker.can_complete() {
+        completed.push(worker.complete());
+      }
+    }
+    completed
+  }
+}
+
+fn part2(dep_map: &DependencyMap) -> u32 {
+  let mut seen: HashSet<char> = HashSet::new();
+  let mut completed: HashSet<char> = HashSet::new();
+  let mut next: Vec<char> = vec![];
+
+  let mut pool = Pool::new(5);
+  while completed.len() < dep_map.len() {
+    next.sort();
+    while pool.can_work() && !next.is_empty() {
+      let v = next.remove(0);
+      pool.work(v);
+      seen.insert(v);
+    }
+
+    for ch in pool.tick() {
+      completed.insert(ch);
+    }
+
+    for (&step, deps) in dep_map {
+      if seen.contains(&step) || next.contains(&step) || completed.contains(&step) {
+        continue;
+      }
+      let deps_len = deps.iter().filter(|d| !completed.contains(d)).count();
+      if deps_len == 0 {
+        next.push(step);
+      }
+    }
+  }
+  pool.elapsed - 1
 }
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ mod day7;
 mod utils;
 
 fn main() {
-  day1::run();
-  day2::run();
-  day3::run();
-  day4::run();
-  day5::run();
-  day6::run();
+  // day1::run();
+  // day2::run();
+  // day3::run();
+  // day4::run();
+  // day5::run();
+  // day6::run();
   day7::run();
 }
